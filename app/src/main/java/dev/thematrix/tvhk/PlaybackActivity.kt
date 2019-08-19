28232622236591c04dafd4bd27e28ee8dfdd9ef7
y2kbug <19472561+y2kbug-hk@users.noreply.github.com>
2019-08-19 23:09:26 +0800

    apply theme on the fly

--- app/src/main/java/dev/thematrix/tvhk/PlaybackActivity.kt
+++ app/src/main/java/dev/thematrix/tvhk/PlaybackActivity.kt
@@ -2,16 +2,26 @@ package dev.thematrix.tvhk
 
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.content.res.Configuration
 import android.os.Bundle
+import android.util.Log
 import android.view.KeyEvent
 import android.view.MotionEvent
+import android.view.View
 import android.view.WindowManager
 import androidx.fragment.app.FragmentActivity
 import kotlin.math.abs
+import android.app.PictureInPictureParams
+import android.util.Rational
+
 
 class PlaybackActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.tv)
         super.onCreate(savedInstanceState)
+
+        this.setTheme(android.R.style.Theme_Black_NoTitleBar_Fullscreen)
 
         if (savedInstanceState == null) {
             supportFragmentManager
@@ -28,13 +38,44 @@ class PlaybackActivity : FragmentActivity() {
 
     override fun onPause() {
         super.onPause()
-        saveState()
+
+//        if (!(TVHandler.SDK_VER >= 24 && this.isInPictureInPictureMode)) {
+//            PlaybackVideoFragment().pause()
+            saveState()
+//        }
     }
 
     override fun onResume() {
         super.onResume()
         restoreState()
     }
+
+//    override fun onConfigurationChanged(newConfig: Configuration) {
+//        super.onConfigurationChanged(newConfig)
+//        adjustScreen(resources.configuration)
+//    }
+
+//    override fun onWindowFocusChanged(hasFocus: Boolean) {
+//        super.onWindowFocusChanged(hasFocus)
+//
+//        if (hasFocus) {
+//            adjustScreen(resources.configuration)
+//        }
+//    }
+
+//    override fun onPictureInPictureModeChanged(isInPictureInPictureMode: Boolean, newConfig: Configuration) {
+//        super.onPictureInPictureModeChanged(isInPictureInPictureMode, newConfig)
+//
+//        if (!isInPictureInPictureMode) {
+//            adjustScreen(newConfig)
+//        }
+//    }
+
+//    override fun onUserLeaveHint() {
+//        super.onUserLeaveHint()
+//
+//        pip()
+//    }
 
     private fun saveState(){
         paused = true
@@ -106,9 +147,10 @@ class PlaybackActivity : FragmentActivity() {
                             channelSwitch("NEXT", true)
                         }
                     }else{
-//                        if (changeInY < 0) { // up
-//                        }else if (changeInY > 0) { // down
-//                        }
+                        if (changeInY < 0) { // up
+                        }else if (changeInY > 0) { // down
+//                            pip()
+                        }
                     }
                 }
             }
@@ -125,6 +167,27 @@ class PlaybackActivity : FragmentActivity() {
         this.setResult(Activity.RESULT_OK, intent)
         finish()
     }
+
+//    fun pip(){
+//        if (TVHandler.SDK_VER >= 26 && packageManager.hasSystemFeature(PackageManager.FEATURE_PICTURE_IN_PICTURE)) {
+//            val params = PictureInPictureParams
+//                .Builder()
+//                .setAspectRatio(Rational(16, 9))
+//                .build()
+//            enterPictureInPictureMode(params)
+//        }
+//    }
+
+//    fun adjustScreen(config: Configuration){
+//        val decorView = window.decorView
+//        if (config.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+//            decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+//
+//        } else {
+//            decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//
+//        }
+//    }
 
     companion object{
         private var paused: Boolean = false
